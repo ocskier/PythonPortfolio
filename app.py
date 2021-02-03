a51d3ba0a213dd7b62f63cec9cc7e96d5473192b
Jon Jackson <ocskier@gmail.com>
2021-02-02 19:01:00 -0500

    feat: add about template with dynamic rendering

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='templates');
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def serveHomePage(): 
@@ -10,5 +10,4 @@ def serveHomePage():
 @app.route('/about')
 def serveAboutPage(): 
     return render_template('about.html', name='Jon')
-
 
