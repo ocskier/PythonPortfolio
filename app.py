6603185797306a8168155fe1451247ffa9feeeb5
Jon Jackson <ocskier@gmail.com>
2021-02-03 23:55:58 -0500

    feat: add dotenv, static files, more templates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def serveHomePage(): 
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def serveAboutPage(): 
